9ef341af9bab7c0f6899a3b0ed43f48285b93fe1
Peter Ayeni <ayenicology@gmail.com>
2019-11-01 17:11:29 +0000

    finish methods

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -12,7 +12,7 @@ class VentureCapitalist
     @@all
   end
 
-  def fundings
+  def funding_rounds
     FundingRound.all.select {|funding| funding.venture_capitalist == self}
   end
 
@@ -20,5 +20,23 @@ class VentureCapitalist
     high_worths = all.select { |venture| venture.total_worth > 1_000_000_000 }
     high_worths.map{|capitalist| capitalist.name}
   end
+
+  def offer_contract(startup, type, investment)
+    FoundingRound.new(startup: startup, type: type, investment: investment, venture_capitalist: self)
+  end
+
+  def portfolio
+    funding_rounds.map{|fund| fund.startup }.uniq
+  end
+
+  def biggest_investment
+    funding_rounds.map {|fund| fund.investment}.max
+  end
+
+  def invested(domain)
+    invested_in  = funding_rounds.find_all{|fund| fund.startup.domain == domain}
+    invested_in.reduce(0){|total, s| total += s.investment }
+  end
+
 
 end
